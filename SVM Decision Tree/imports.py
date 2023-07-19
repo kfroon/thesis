490cae8678915afd2b16cbf50235d774aa2138d2
kfroon <kfroon@outlook.com>
2023-07-19 11:23:54 +0200

    required dependencies

---
+++ SVM Decision Tree/imports.py
@@ -0,0 +1,20 @@
+import sklearn
+import pandas as pd 
+import numpy as np
+from IPython.display import Image
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier 
+import six
+import sys
+#from interpretableai import iai
+import matplotlib.pyplot as plt
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix, classification_report
+from sklearn import metrics 
+from sklearn.preprocessing import StandardScaler
+import pickle as pkl
+from sklearn.svm import SVC 
+from collections import Counter
+
+
